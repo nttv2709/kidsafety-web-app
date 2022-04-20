43957c53b67e64051fb4d0a0296789f098d205fe
nttvy2709 <vyngo2709@gmail.com>
2022-04-20 12:51:35 +0700

    alert baby crying

--- src/views/status-page/SoundStatus.vue
+++ src/views/status-page/SoundStatus.vue
@@ -1,9 +1,17 @@
 <template>
-<div class="crying" v-if="crying">
-<div class="alert alert-success" role="alert">
-  <h4 class="alert-heading">SOS</h4>
-</div>
+<div class="modal" id="id1">
+  <div class="modal-dialog position-absolute w-75 top-50 start-50 translate-middle">
+    <div class="modal-content " style="border-radius: 25px">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" @click="hiddenNotify" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Baby is crying</p>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -14,6 +22,7 @@ export default {
   data () {
     return {
       currentValue: '',
+      sound: [],
       crying: false
     }
   },
@@ -21,10 +30,25 @@ export default {
     onValue(ref(StatusDataService.getAll(), '/iot/sound'), (snapshot) => {
       const data = snapshot.val()
       this.currentValue = data.value
+      this.sound.push(this.currentValue)
       if (this.currentValue >= 50) {
         this.crying = true
+        this.displayNotify()
       }
     })
+  },
+  methods: {
+    displayNotify () {
+      document.getElementById('id1').style.display = 'block'
+    },
+    hiddenNotify () {
+      document.getElementById('id1').style.display = 'none'
+    }
   }
 }
 </script>
+<style scoped>
+  .modal {
+    position: absolute;
+  }
+</style>
