bae348b2208f3789bed12a3f80d1b8caeec3f6c4
nttvy2709 <vyngo2709@gmail.com>
2022-04-22 11:16:40 +0700

    Add more news

--- src/views/status-page/NewsList.vue
+++ src/views/status-page/NewsList.vue
@@ -10,6 +10,17 @@
     </div>
   </div>
 </div>
+<div class="news shadow p-3 row g-0 mt-3 position-relative">
+  <div class="col-md-6 mb-md-0 p-md-4">
+    <img src="../../assets/news1.jpg" class="d-block w-100" alt="...">
+  </div>
+  <div class="col-md-6 p-4 ps-md-0 d-flex align-items-center">
+    <div class="content p-3">
+    <p>The best temperature for babies is between 68 to 72 degrees Fahrenheit or 20 to 22 degrees Celsius</p>
+    <a href="https://www.webmd.com/baby/what-is-the-right-room-temperature-for-a-baby" class="btn rounded-pill stretched-link mt-2">More information</a>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -27,7 +38,7 @@ img {
   border-radius: 50px;
 }
 p {
-  font-size: 1.5rem;
+  font-size: 1.25rem;
 }
 .btn {
   background: black;
